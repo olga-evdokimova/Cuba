4d2a7074b3933079c3092ed506dcf9e2d8838702
pavedom13 <pavedom13@gmail.com>
2020-12-14 11:15:46 +0300

    photo+slider готов

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./image/content/Arrow-prev.svg" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./image/content/Arrow-next.svg" alt="bext"></button>',
+    autoplay: true,//пролистывание по умолчнию 3 сек
+    fade: true,// эффект возникновения слайда вместо перелистывания
+  
+});
